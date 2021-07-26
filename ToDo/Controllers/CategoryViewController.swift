bde521388cfffb750d7cdc9bc5a78ee16a993cf9
Rituraj <riturajnambiar98@gmail.com>
2021-07-26 17:36:01 +0530

    Remove NSCoder, Introducing Core Data, Carrying out CRUD(Create, Read, Update, Delete), Add CategoryTableViewController.

---
+++ ToDo/Controllers/CategoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewController.swift
+//  ToDo
+//
+//  Created by Rituraj R on 26/07/21.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    //MARK: - Add New Categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+   
+    
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+}
